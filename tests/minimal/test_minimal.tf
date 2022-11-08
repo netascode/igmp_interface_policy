c91baae8237f0daadbf03314c06c89d2bfb6159c
jgomezve <jgomezve@cisco.com>
2022-11-08 16:46:35 -0500

    Extend minimal tests

--- tests/minimal/test_minimal.tf
+++ tests/minimal/test_minimal.tf
@@ -45,4 +45,93 @@ resource "test_assertions" "igmpIfPol" {
     got         = data.aci_rest_managed.igmpIfPol.content.descr
     want        = ""
   }
+
+  equal "grpTimeout" {
+    description = "grpTimeout"
+    got         = data.aci_rest_managed.igmpIfPol.content.grpTimeout
+    want        = "260"
+  }
+
+  equal "ifCtrl" {
+    description = "ifCtrl"
+    got         = data.aci_rest_managed.igmpIfPol.content.ifCtrl
+    want        = "allow-v3-asm,fast-leave,rep-ll"
+  }
+
+  equal "lastMbrCnt" {
+    description = "lastMbrCnt"
+    got         = data.aci_rest_managed.igmpIfPol.content.lastMbrCnt
+    want        = "2"
+  }
+
+  equal "lastMbrRespTime" {
+    description = "lastMbrRespTime"
+    got         = data.aci_rest_managed.igmpIfPol.content.lastMbrRespTime
+    want        = "1"
+  }
+
+  equal "querierTimeout" {
+    description = "querierTimeout"
+    got         = data.aci_rest_managed.igmpIfPol.content.querierTimeout
+    want        = "255"
+  }
+
+  equal "queryIntvl" {
+    description = "queryIntvl"
+    got         = data.aci_rest_managed.igmpIfPol.content.queryIntvl
+    want        = "125"
+  }
+
+  equal "robustFac" {
+    description = "robustFac"
+    got         = data.aci_rest_managed.igmpIfPol.content.robustFac
+    want        = "2"
+  }
+
+  equal "rspIntvl" {
+    description = "rspIntvl"
+    got         = data.aci_rest_managed.igmpIfPol.content.rspIntvl
+    want        = "25"
+  }
+
+  equal "startQueryCnt" {
+    description = "startQueryCnt"
+    got         = data.aci_rest_managed.igmpIfPol.content.startQueryCnt
+    want        = "2"
+  }
+
+  equal "startQueryIntvl" {
+    description = "startQueryIntvl"
+    got         = data.aci_rest_managed.igmpIfPol.content.startQueryIntvl
+    want        = "31"
+  }
+
+  equal "ver" {
+    description = "ver"
+    got         = data.aci_rest_managed.igmpIfPol.content.ver
+    want        = "v2"
+  }
+}
+
+
+data "aci_rest_managed" "igmpStateLPol" {
+  dn = "${dat.aci_rest_managed.igmpIfPol.id}/igmpstateLPol"
+
+  depends_on = [module.main]
+}
+
+resource "test_assertions" "igmpStateLPol" {
+  component = "igmpStateLPol"
+
+  equal "max" {
+    description = "max"
+    got         = data.aci_rest_managed.igmpStateLPol.content.max
+    want        = "unlimited"
+  }
+
+  equal "rsvd" {
+    description = "rsvd"
+    got         = data.aci_rest_managed.igmpStateLPol.content.rsvd
+    want        = "undefined"
+  }
 }
